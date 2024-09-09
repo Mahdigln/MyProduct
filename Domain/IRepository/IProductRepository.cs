feba3024bcad977156772945d1f8a56b0194b65c
Mahdi Golestan <Mahdi Golestan>
2024-09-09 20:59:57 +0330

    add product repository

---
+++ Domain/IRepository/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.DependencyInjection;
+using Domain.Abstractions;
+using Domain.Entities.Product;
+
+namespace Domain.IRepository;
+
+public interface IProductRepository : IRepositoryBase<Product>, IScopeLifetime
+{
+}
---
+++ Infrastructure/Repositories/Product/ProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.IRepository;
+using Infrastructure.Context;
+
+namespace Infrastructure.Repositories.Product;
+
+public sealed class ProductRepository : RepositoryBase<Domain.Entities.Product.Product>, IProductRepository
+{
+    public ProductRepository(MainDbContext dbContext) : base(dbContext)
+    {
+    }
+}
