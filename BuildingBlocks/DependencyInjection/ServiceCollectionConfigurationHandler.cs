75102fac6365004a1afe17e23d6019469a7091f5
Mahdi Golestan <Mahdi Golestan>
2024-09-09 12:12:37 +0330

    add mediateR and Mapster

---
+++ BuildingBlocks/DependencyInjection/ServiceCollectionConfigurationHandler.cs
@@ -0,0 +1,37 @@
+using BuildingBlocks.CQRS;
+using Mapster;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.DependencyInjection;
+public static class ServiceCollectionConfigurationHandler
+{
+    public static IServiceCollection RegisterMapping(this IServiceCollection services)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        TypeAdapterConfig.GlobalSettings.Scan(assemblies);
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+
+        var assemblys = AppDomain.CurrentDomain.GetAssemblies();
+
+
+        services.Scan(selector => selector
+            .FromAssemblies(assemblys)
+            .AddClasses(filter => filter.AssignableTo(typeof(ICommandHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+
+        services.Scan(selector => selector
+            .FromAssemblies(assemblys)
+            .AddClasses(filter => filter.AssignableTo(typeof(IQueryHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithTransientLifetime());
+
+
+        return services;
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,11 @@
+using BuildingBlocks.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.RegisterServices();
+builder.Services.RegisterMapping();
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
