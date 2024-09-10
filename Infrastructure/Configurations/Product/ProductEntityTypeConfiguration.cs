ae40560531f189f53fe972d97b9af3d61fb5ee6f
Mahdi Golestan <Mahdi Golestan>
2024-09-10 14:42:00 +0330

    add QueryFilter

--- Infrastructure/Configurations/Product/ProductEntityTypeConfiguration.cs
+++ Infrastructure/Configurations/Product/ProductEntityTypeConfiguration.cs
@@ -5,34 +5,36 @@ namespace Infrastructure.Configurations.Product;
 
 public class ProductEntityTypeConfiguration : IEntityTypeConfiguration<Domain.Entities.Product.Product>
 {
-    public void Configure(EntityTypeBuilder<Domain.Entities.Product.Product> builder)
-    {
-        ArgumentNullException.ThrowIfNull(builder);
+	public void Configure(EntityTypeBuilder<Domain.Entities.Product.Product> builder)
+	{
+		ArgumentNullException.ThrowIfNull(builder);
 
 
-        builder.ToTable("Products");
+		builder.ToTable("Products");
 
-        builder.Property(p => p.Name)
-            .IsRequired()
-            .HasMaxLength(100);
+		builder.Property(p => p.Name)
+			.IsRequired()
+			.HasMaxLength(100);
 
-        builder.Property(p => p.ManufactureEmail)
-            .IsRequired()
-            .HasMaxLength(100);
+		builder.Property(p => p.ManufactureEmail)
+			.IsRequired()
+			.HasMaxLength(100);
 
-        builder.Property(p => p.ManufacturePhone)
-            .HasMaxLength(11);
+		builder.Property(p => p.ManufacturePhone)
+			.HasMaxLength(11);
 
-        builder.Property(p => p.ProduceDate)
-            .IsRequired();
+		builder.Property(p => p.ProduceDate)
+			.IsRequired();
 
-        builder.HasIndex(p => new { p.ManufactureEmail, p.ProduceDate })
-            .HasDatabaseName("IX_ManufactureEmail_ProduceDate")
-            .IsUnique(true);
+		builder.HasIndex(p => new { p.ManufactureEmail, p.ProduceDate })
+			.HasDatabaseName("IX_ManufactureEmail_ProduceDate")
+			.IsUnique(true);
 
-        builder.HasOne(p => p.User)
-            .WithMany(u => u.Products)
-            .HasForeignKey(p => p.UserId)
-            .OnDelete(DeleteBehavior.Restrict);
-    }
+		builder.HasQueryFilter(p => !p.IsDeleted);
+
+		builder.HasOne(p => p.User)
+			.WithMany(u => u.Products)
+			.HasForeignKey(p => p.UserId)
+			.OnDelete(DeleteBehavior.Restrict);
+	}
 }
