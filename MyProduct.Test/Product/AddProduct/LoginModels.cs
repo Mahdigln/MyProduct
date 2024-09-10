01aacad5a81cf4b0f0aeb1a97a55efefc2108015
Mahdi Golestan <Mahdi Golestan>
2024-09-10 18:30:29 +0330

    init test

---
+++ MyProduct.Test/Product/AddProduct/LoginModels.cs
@@ -0,0 +1,18 @@
+﻿namespace MyProduct.Test.Product.AddProduct;
+
+internal sealed class LoginModels
+{
+	public string UserName { get; set; }
+	public string Password { get; set; }
+}
+
+public sealed class LoginResponse
+{
+	public Accesstoken accessToken { get; set; }
+}
+
+public class Accesstoken
+{
+	public string token { get; set; }
+	public string tokenType { get; set; }
+}
---
+++ MyProduct.Test/ProductApiFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using WebAPI;
+
+namespace MyProduct.Test;
+
+public class ProductApiFactory : WebApplicationFactory<IApiMarker>
+{
+	protected override void ConfigureWebHost(IWebHostBuilder builder)
+	{
+		base.ConfigureWebHost(builder);
+	}
+}
