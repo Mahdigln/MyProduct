cb0ea4df808fe87f6127f09b9b88a0a6588705c9
Mahdi Golestan <Mahdi Golestan>
2024-09-10 13:56:25 +0330

    add auto CreateDateBase

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,12 @@
 using BuildingBlocks.DependencyInjection;
 using Infrastructure;
+using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 builder.Services.RegisterServices();
 builder.Services.RegisterMapping();
@@ -14,49 +17,54 @@ builder.Services.AddEndpointsApiExplorer();
 #region AddSwaggerGen
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 
-    // Configure Swagger to use JWT Bearer Authentication
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your token in the text input below."
-    });
+	// Configure Swagger to use JWT Bearer Authentication
+	c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer",
+		In = ParameterLocation.Header,
+		Description = "Enter 'Bearer' [space] and then your token in the text input below."
+	});
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] { }
-        }
-    });
+	c.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] { }
+		}
+	});
 });
-
-
-
 #endregion
 
-
 var app = builder.Build();
+
+// Ensure database is created/migrated
+using (var scope = app.Services.CreateScope())
+{
+	var dbContext = scope.ServiceProvider.GetRequiredService<MainDbContext>();
+	dbContext.Database.Migrate(); // Migrate or create the database
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
-//app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
